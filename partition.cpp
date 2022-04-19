1b0dff5db1ada3ac655eaabfa245dc396f07568f
John Sanchez <johnsanchez@college.harvard.edu>
2022-04-19 13:36:31 -0400

    pro-gasm

---
+++ partition.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <random>
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        printf("Usage: ./partition 0 algorithm inputfile\n");
+        return 1;
+    }
+
+
+}
